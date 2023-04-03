0eda27bc774e30c8c878d393b02ffff3a62ae19d
pavramenko <p.a.avramenko@gmail.com>
2023-04-03 14:35:36 +0300

    feat(sign up form): add new fields, styles

---
+++ src/app/sign-up/models/country.model.ts
@@ -0,0 +1,6 @@
+export interface ICountry {
+  name: {
+    common: string;
+  };
+  flag: string;
+}
---
+++ src/app/sign-up/services/sign-up.service.ts
@@ -0,0 +1,19 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { ICountry } from '../models/country.model';
+
+@Injectable()
+export class SignUpService {
+  http = inject(HttpClient);
+
+  getCountries(): Observable<ICountry[]> {
+    return this.http.get<ICountry[]>('https://restcountries.com/v3.1/all').pipe(
+      map((response: ICountry[]) => {
+        return response.sort((a: ICountry, b: ICountry) =>
+          a.name.common.localeCompare(b.name.common)
+        );
+      })
+    );
+  }
+}

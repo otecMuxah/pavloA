1d031421574f62861db15bcd6a0c9f8488fd814b
Pavlo Avramenko <48721238+avramenkop@users.noreply.github.com>
2023-05-04 12:29:20 +0300

    feat(sign in form): add styles (#11)
    * feat(sign in form): add styles
    
    * feat(sign up form): add country search (#10)
    
    * feat(sign up form): add country search
    
    * feat(sign up form): minor fixes
    
    * feat(sign in form): add styles
    
    * feat(sign in form): update after review

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';

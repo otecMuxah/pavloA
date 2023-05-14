697904d7f9742b2ec56ae719b20907551bf625fa
Pavlo Avramenko <48721238+avramenkop@users.noreply.github.com>
2023-05-15 02:16:09 +0300

    feat(app): add routing (#12)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SignInComponent } from './auth/sign-in/sign-in.component';
-import { SignUpComponent } from './auth/sign-up/sign-up.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'sign-in' },
-  { path: 'sign-in', title: 'Sign-in', component: SignInComponent },
-  { path: 'sign-up', title: 'Sign-up', component: SignUpComponent },
+  {
+    path: '',
+    pathMatch: 'full',
+    title: 'StarWars fans resource',
+    loadChildren: () =>
+      import('./overview/overview.module').then((m) => m.OverviewModule),
+  },
+  {
+    path: 'sign-in',
+    title: 'Sign-in',
+    loadChildren: () =>
+      import('./auth/sign-in/sign-in.module').then((m) => m.SignInModule),
+  },
+  {
+    path: 'sign-up',
+    title: 'Sign-up',
+    loadChildren: () =>
+      import('./auth/sign-up/sign-up.module').then((m) => m.SignUpModule),
+  },
+  {
+    path: 'page-not-found',
+    title: 'Page not found',
+    loadChildren: () =>
+      import('./not-found/not-found.module').then((m) => m.NotFoundModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'page-not-found',
+  },
 ];
 
 @NgModule({
